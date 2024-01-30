34e773e3793359f39a578c404ddc86b46f0fa71e
Rastsislau Lipski <rlipski@elinext.com>
2024-01-30 15:03:31 +0300

    Initial commit

---
+++ src/main/kotlin/rastsislaux/pgi/PrometheusGrahanaInvestigateApplication.kt
@@ -0,0 +1,11 @@
+package rastsislaux.pgi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrometheusGrahanaInvestigateApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrometheusGrahanaInvestigateApplication>(*args)
+}
---
+++ src/test/kotlin/rastsislaux/pgi/PrometheusGrahanaInvestigateApplicationTests.kt
@@ -0,0 +1,13 @@
+package rastsislaux.pgi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrometheusGrahanaInvestigateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
